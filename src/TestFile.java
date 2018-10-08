c9ad55b7464ed7e756ad91c371a0f64a9395e9de
10760 <zchen5143@bths.edu>
2018-10-07 23:50:56 -0400

    Final VERSION

--- src/TestFile.java
+++ src/TestFile.java
@@ -6,6 +6,6 @@ public class TestFile{
         System.out.println(AChenLib.leastCommonMultiple(12,20,30)); //find the least common multiple of 3 numbers
         AChenLib.primePrinter(20); //prints the first "num" prime numbers
         System.out.println(AChenLib.isPalindrome("racecar")); //checks if the inputted word is a palindrome
-         System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin")); //encodes a message using vigenere cipher
+        System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin")); //encodes a message using vigenere cipher
     }
 }
