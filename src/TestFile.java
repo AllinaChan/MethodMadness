863353f346802cf106db2622ba9d3a5418b2df1c
10760 <zchen5143@bths.edu>
2018-10-07 23:50:44 -0400

    Final VERSION

--- src/TestFile.java
+++ src/TestFile.java
@@ -2,10 +2,10 @@ public class TestFile{
     //Allen Chen Period 9
     public static void main(String[] args){
         System.out.println("Hello Method Madness");
-        System.out.println(AChenLib.quadSolver(1,7,6));
-        System.out.println(AChenLib.leastCommonMultiple(12,20,30));
-        AChenLib.primePrinter(20);
-        System.out.println(AChenLib.isPalindrome("racecar"));
-         System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin"));
+        System.out.println(AChenLib.quadSolver(1,7,6)); //solves a quadratic equation
+        System.out.println(AChenLib.leastCommonMultiple(12,20,30)); //find the least common multiple of 3 numbers
+        AChenLib.primePrinter(20); //prints the first "num" prime numbers
+        System.out.println(AChenLib.isPalindrome("racecar")); //checks if the inputted word is a palindrome
+         System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin")); //encodes a message using vigenere cipher
     }
 }
