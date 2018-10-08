ee9001cfe4738b64568e1d907ffa0475b3b54f02
10760 <zchen5143@bths.edu>
2018-10-07 23:16:30 -0400

    with vigCipher

--- src/TestFile.java
+++ src/TestFile.java
@@ -2,10 +2,10 @@ public class TestFile{
     //Allen Chen Period 9
     public static void main(String[] args){
         System.out.println("Hello Method Madness");
-        //System.out.println(AChenLib.quadSolver(1,7,6));
-        //System.out.println(AChenLib.leastCommonMultiple(12,20,30));
-        //AChenLib.primePrinter(20);
-        //System.out.println(AChenLib.isPalindrome("racecar"));
-       //System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin"));
+        System.out.println(AChenLib.quadSolver(1,7,6));
+        System.out.println(AChenLib.leastCommonMultiple(12,20,30));
+        AChenLib.primePrinter(20);
+        System.out.println(AChenLib.isPalindrome("racecar"));
+        System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin"));
     }
 }
