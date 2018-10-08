49f0f514922e77c66c105480b64bc670b7e117ab
10760 <zchen5143@bths.edu>
2018-10-07 23:16:56 -0400

    Final VERSION

--- src/TestFile.java
+++ src/TestFile.java
@@ -6,6 +6,6 @@ public class TestFile{
         System.out.println(AChenLib.leastCommonMultiple(12,20,30));
         AChenLib.primePrinter(20);
         System.out.println(AChenLib.isPalindrome("racecar"));
-        System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin"));
+         System.out.println(AChenLib.vigCipher("Method Madness", "MrLevin"));
     }
 }
